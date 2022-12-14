32ebba49acdbeff874b05f73d3c02c5cefeecb92
Tấn Minh Lương <tanminhluong99@gmail.com>
2022-12-14 11:38:30 +0700

    fixed socketio

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const server = app.listen(PORT, console.log("Server start on port", PORT));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-chat-orsr.onrender.com/",
   },
 });
 
