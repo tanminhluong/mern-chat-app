cf5fd9d3f127dbbe1297695cff10a287ae1253e1
Tấn Minh Lương <tanminhluong99@gmail.com>
2022-12-14 11:24:10 +0700

    allowed cors

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,13 @@ const chatRoutes = require("./routes/chatRoutes.js");
 const messageRoutes = require("./routes/messageRoutes.js");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/chats", chatRoutes);
