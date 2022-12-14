04a1535adb010afc17558694a5fbe83663c73bb0
Tấn Minh Lương <tanminhluong99@gmail.com>
2022-12-14 14:54:38 +0700

    ...

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 connectDB();
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-chat-orsr.onrender.com/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
